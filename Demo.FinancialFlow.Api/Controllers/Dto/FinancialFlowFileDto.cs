a6273953627ad873e690f97c79b88441fa212b45
Ocean Barras <ocean.barras@hotmail.com>
2025-10-20 16:34:21 -0400

    setup route to receive file

---
+++ Demo.FinancialFlow.Api/Controllers/Dto/FinancialFlowFileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.FinancialFlow.Api.Controllers.Dto
+{
+    /// <summary>
+    /// This class is a simple DTO for the file upload in the FinancialFlowController
+    /// 
+    /// It circumvent a bug that occurs with swagger UI where IFormFile route argument cannot be rendered
+    /// </summary>
+    public class FinancialFlowFileDto
+    {
+        public required IFormFile File { get; set; }
+    }
+}
--- Demo.FinancialFlow.Api/Controllers/FinancialFlowController.cs
+++ Demo.FinancialFlow.Api/Controllers/FinancialFlowController.cs
@@ -1,10 +1,13 @@
-﻿using Demo.FinancialFlow.Infrastructure;
+﻿using Demo.FinancialFlow.Api.Commands;
+using Demo.FinancialFlow.Api.Controllers.Dto;
+using Demo.FinancialFlow.Infrastructure;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.FinancialFlow.Api.Controllers
 {
     [Route("api/[controller]")]
-    public class FinancialFlowController(FinancialFlowContext dbContext): ControllerBase
+    public class FinancialFlowController(FinancialFlowContext dbContext, IMediator mediator): ControllerBase
     {
         [HttpGet("health")]
         public async Task<ActionResult<bool>> HealthCheckAsync()
@@ -14,6 +17,32 @@ namespace Demo.FinancialFlow.Api.Controllers
             //dbContext.FinancialFlows.Add(new Domain.FinancialFlow(100.0f, DateTime.UtcNow, "test123", Domain.FlowType.Past, "testsub123"));
 
             //await dbContext.SaveChangesAsync();
+
+            return Ok(true);
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile([FromForm] FinancialFlowFileDto financialFlowFile)
+        {
+            var file = financialFlowFile.File;
+
+            if (financialFlowFile == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            if (file.Length > 10 * 1024 * 1024)
+                return BadRequest("File too large.");
+
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            var permittedExtensions = new[] { ".csv"};
+            if (!permittedExtensions.Contains(ext))
+                return BadRequest("Invalid file type.");
+
+            var processFinancialFlowFileCommand = new ProcessFinancialFlowFileCommand(file);
+
+            await mediator.Send(processFinancialFlowFileCommand);
 
             return Ok(true);
         }
