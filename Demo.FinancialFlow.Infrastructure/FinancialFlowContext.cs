72154a9649b776929ad2ebec6d74e8048e0e1a42
Ocean Barras <ocean.barras@hotmail.com>
2025-10-20 12:54:27 -0400

    add sql context in infrastructure layer

---
+++ Demo.FinancialFlow.Infrastructure/FinancialFlowContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.FinancialFlow.Infrastructure
+{
+    public class FinancialFlowContext(DbContextOptions<FinancialFlowContext> dbContextOptions) : DbContext(dbContextOptions)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("demo");
+        }
+
+    }
+}
