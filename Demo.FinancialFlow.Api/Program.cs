cddb41e203846c90fb4c14583c63df671393c521
Ocean Barras <ocean.barras@hotmail.com>
2025-10-20 12:54:39 -0400

    connect to sql context in api layer

--- Demo.FinancialFlow.Api/Program.cs
+++ Demo.FinancialFlow.Api/Program.cs
@@ -1,3 +1,6 @@
+using Demo.FinancialFlow.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 namespace Demo.FinancialFlow.Api
 {
     public class Program
@@ -7,6 +10,26 @@ namespace Demo.FinancialFlow.Api
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<FinancialFlowContext>(options =>
+            {
+                var currentLogLevel = builder.Configuration.GetValue<LogLevel>("Logging:LogLevel:Microsoft.EntityFrameworkCore");
+
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Sql"), sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(10),
+                        errorNumbersToAdd: null);
+                })
+                .LogTo(Console.WriteLine, currentLogLevel);
+
+                if (currentLogLevel == LogLevel.Debug)
+                {
+                    options.EnableSensitiveDataLogging();
+                    options.EnableDetailedErrors();
+                }
+            });
 
             var app = builder.Build();
 
