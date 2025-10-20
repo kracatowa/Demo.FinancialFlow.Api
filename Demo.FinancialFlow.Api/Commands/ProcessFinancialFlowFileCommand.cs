33f83890348c80cad616bd17a0ea5fd339587622
Ocean Barras <ocean.barras@hotmail.com>
2025-10-20 16:34:39 -0400

    add command handler for adding all financial flows

---
+++ Demo.FinancialFlow.Api/Commands/ProcessFinancialFlowFileCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Demo.FinancialFlow.Api.Commands
+{
+    public class ProcessFinancialFlowFileCommand(IFormFile file) : IRequest<Unit>
+    {
+        public IFormFile File { get; } = file;
+    }
+}
---
+++ Demo.FinancialFlow.Api/Commands/ProcessFinancialFlowFileCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Demo.FinancialFlow.Api.Services.File;
+using Demo.FinancialFlow.Infrastructure.Repositories;
+using MediatR;
+
+namespace Demo.FinancialFlow.Api.Commands
+{
+    public class ProcessFinancialFlowFileCommandHandler(IFileProcessor fileProcessor,
+                                                        IFinancialFlowRepository financialFlowRepository) : IRequestHandler<ProcessFinancialFlowFileCommand, Unit>
+    {
+        public async Task<Unit> Handle(ProcessFinancialFlowFileCommand request, CancellationToken cancellationToken)
+        {
+            using var stream = new MemoryStream();
+            await request.File.CopyToAsync(stream, cancellationToken);
+
+            var extension = Path.GetExtension(request.File.FileName);
+            var financialFlows = fileProcessor.ProcessFinancialFlow(stream, extension);
+
+            foreach(var financialFlow in financialFlows)
+            {
+                await financialFlowRepository.AddAsync(financialFlow);
+            }
+            
+            await financialFlowRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
