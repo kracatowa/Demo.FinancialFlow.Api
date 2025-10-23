73c7f65fafb3853b573d090a682c686b3cb300d4
Ocean Barras <ocean.barras@hotmail.com>
2025-10-23 15:28:59 -0400

    move financialflow repository inside the domain project

---
+++ Demo.FinancialFlow.Domain/FinancialFlowAggregate/IFinancialFlowRepository.cs
@@ -0,0 +1,9 @@
+﻿using Demo.FinancialFlow.Domain.Seedwork;
+
+namespace Demo.FinancialFlow.Domain.FinancialFlowAggregate
+{
+    public interface IFinancialFlowRepository : IRepository
+    {
+        Task AddAsync(FinancialFlow financialFlow);
+    }
+}
