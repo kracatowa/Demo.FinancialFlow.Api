1daf8914e1ff36d70c3d78bad02ff8f54432bc54
Ocean Barras <ocean.barras@hotmail.com>
2025-10-27 15:25:23 -0400

    add domain event handler for file status change

---
+++ Demo.FinancialFlow.Api/DomainEvents/FinancialFlowFileProcessedDomainEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Demo.FinancialFlow.Domain.FileAggregate;
+using MediatR;
+
+namespace Demo.FinancialFlow.Api.DomainEvents
+{
+    public class FinancialFlowFileProcessedHandler : INotificationHandler<FinancialFlowFileProcessedDomainEvent>
+    {
+        private readonly IFinancialFlowFileAuditRepository _repository;
+        public FinancialFlowFileProcessedHandler(IFinancialFlowFileAuditRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(FinancialFlowFileProcessedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            var file = await _repository.GetByStorageFileId(notification.FinancialFlowFileId) 
+                        ?? throw new ArgumentException($"Financial flow file with storage id {notification.FinancialFlowFileId} not found.");
+
+            file.MarkAsProcessed();
+
+            await _repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
