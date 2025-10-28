2155aa3aae95347e6483b81b1b9ea19543ffd358
Ocean Barras <ocean.barras@hotmail.com>
2025-10-28 08:45:31 -0400

    simplify commands to only have operations that are needed to be tracked by database

---
+++ Demo.FinancialFlow.Api/Commands/ProcessBatchFinancialFlowsCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Demo.FinancialFlow.Domain.FileAggregate;
+using Demo.FinancialFlow.Domain.FinancialFlowAggregate;
+using MediatR;
+
+namespace Demo.FinancialFlow.Api.Commands
+{
+    public class ProcessBatchFinancialFlowsCommandHandler(IFinancialFlowRepository financialFlowRepository,
+                                                        IMediator mediator) : IRequestHandler<ProcessBatchFinancialFlowsCommand, Unit>
+    {
+        public async Task<Unit> Handle(ProcessBatchFinancialFlowsCommand request, CancellationToken cancellationToken)
+        {
+            foreach (var financialFlow in request.FinancialFlowTransactions)
+            {
+                await financialFlowRepository.AddAsync(financialFlow);
+            }
+
+            await financialFlowRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+            await mediator.Publish(new FinancialFlowFileProcessedDomainEvent(request.FinancialStorageId), cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
